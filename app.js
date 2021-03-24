45dca173c89203d7700c1fd8b1cc1eb561366e2e
Georges <cameroon_football@hotmail.de>
2021-03-23 23:20:28 -0400

    Added styles and reworked app.js to work with DOM

--- app.js
+++ app.js
@@ -1,38 +1,87 @@
+/*const btnRock = document.querySelector("#btnRock");
+btnRock.addEventListener("click", e => {
+    alert("Rock");
+});
+
+const btnPaper = document.querySelector("#btnPaper");
+btnPaper.addEventListener("click", e => {
+    alert("Paper");
+});
+
+const btnScissors = document.querySelector("#btnScissors");
+btnScissors.addEventListener("click", e => {
+    alert("Scissors");
+});*/
+
 let userInput;
+const body = document.querySelector("body");
+const container = document.querySelector("#container");
+const buttons = document.querySelectorAll("button");
+const content = document.createElement('div');
+content.classList.add('content');
+
+function disableButton() {
+    buttons.diabled = true;
+}
 
 // Function that manages the whole game
 function game() {
     let roundWinner;
     let playerScore = 0;
     let computerScore = 0;
+    let round = 0;
 
-    for (let i = 0; i < 5; i++) {
-        // Ask user for input
-        userInput = prompt("Enter your choice");
+    buttons.forEach((button) => {
+        button.addEventListener("click", e => {
+            userInput = e.target.textContent;
 
-        // Store the userinput and computer choice in a variable
-        const playerSelection = userInput;
-        const computerSelection = computerPlay();
+            // Store the userinput and computer choice in a variable
+            const playerSelection = userInput;
+            const computerSelection = computerPlay();
 
-        // call the playRound function for a single round and store the winner in the roundWinner variable
-        roundWinner = playRound(playerSelection, computerSelection);
+            // call the playRound function for a single round and store the winner in 
+            // the roundWinner variable
+            roundWinner = playRound(playerSelection, computerSelection);
 
-        if (roundWinner == "player") {
-            playerScore++;
-        } else if (roundWinner == "computer") {
-            computerScore++;
-        }
-    }
+            if (roundWinner == "player") {
+                playerScore++;
+            } else if (roundWinner == "computer") {
+                computerScore++;
+            }
 
-    if (playerScore > computerScore) {
-        console.log("You are the winner.");
-    } else if (playerScore < computerScore) {
-        console.log("The computer won.")
-    } else {
-        console.log("It's a tie between you two.");
-    }
+            round++;
 
-    console.log("Final score: You: " + playerScore + " - Computer: " + computerScore);
+            if (round == 5) {
+                if (playerScore > computerScore) {
+                    const playerWinner = document.createElement("p");
+                    playerWinner.textContent = "You are the winner.";
+                    body.appendChild(playerWinner);
+                } else if (playerScore < computerScore) {
+                    const computerWinner = document.createElement("p");
+                    computerWinner.textContent = "The computer won.";
+                    body.appendChild(computerWinner);
+                } else {
+                    const tie = document.createElement("p");
+                    tie.textContent = "It's a tie between you two.";
+                    body.appendChild(tie);
+                }
+                
+                const finalScore = document.createElement("p");
+                finalScore.textContent = `Final score: You:  ${playerScore} - Computer: ${computerScore}`;
+                body.appendChild(finalScore);
+
+                container.classList.add("disabledButton");
+                
+                const reloadPage = document.createElement("button");
+                reloadPage.textContent = "Retry?";
+                body.appendChild(reloadPage);
+
+                reloadPage.addEventListener("click", () => {
+                    window.location.reload();
+                });
+            }
+        });
+    });
 }
 
 // Function to create the random computer choice
@@ -48,36 +97,54 @@ function playRound(playerSelection, computerSelection) {
     let playerWin = 0;
     let computerWin = 0;
 
-    // Check if the player entered "rock"
-    if (playerSelection.toUpperCase() == "Rock".toUpperCase() && computerSelection.toUpperCase() == "Paper".toUpperCase()) {
-        console.log("You lose! Paper beats Rock");
+    // Check if the player chose "rock"
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        const rockLose = document.createElement("p");
+        rockLose.textContent = "You lose! Paper beats Rock";
+        container.appendChild(rockLose);
         computerWin++;
-    } else if (playerSelection.toUpperCase() == "Rock".toUpperCase() && computerSelection.toUpperCase() == "Scissors".toUpperCase()) {
-        console.log("You win! Rock beats Scissors");
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        const rockWin = document.createElement("p");
+        rockWin.textContent = "You win! Rock beats Scissors";
+        container.appendChild(rockWin);
         playerWin++;
-    } else if (playerSelection.toUpperCase() == "Rock".toUpperCase() && computerSelection.toUpperCase() == "Rock".toUpperCase()) {
-        console.log("It's a tie");
+    } else if (playerSelection == "Rock" && computerSelection == "Rock") {
+        const rockTie = document.createElement("p");
+        rockTie.textContent = "It's a tie";
+        container.appendChild(rockTie);
     }
 
-    // check if the player entered "paper"
-    if (playerSelection.toUpperCase() == "Paper".toUpperCase() && computerSelection.toUpperCase() == "Paper".toUpperCase()) {
-        console.log("It's a tie");
-    } else if (playerSelection.toUpperCase() == "Paper".toUpperCase() && computerSelection.toUpperCase() == "Scissors".toUpperCase()) {
-        console.log("You Lose! Scissors beats Paper");
+    // Check if the player chose "paper"
+    if (playerSelection == "Paper" && computerSelection == "Paper") {
+        const paperTie = document.createElement("p");
+        paperTie.textContent = "It's a tie";
+        container.appendChild(paperTie);
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        const paperLose = document.createElement("p");
+        paperLose.textContent = "You Lose! Scissors beats Paper";
+        container.appendChild(paperLose);
         computerWin++;
-    } else if (playerSelection.toUpperCase() == "Paper".toUpperCase() && computerSelection.toUpperCase() == "Rock".toUpperCase()) {
-        console.log("You win! Paper beats Rock");
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        const paperWin = document.createElement("p");
+        paperWin.textContent = "You win! Paper beats Rock";
+        container.appendChild(paperWin);
         playerWin++;
     }
     
-    // check if the player entered "scissors"
-    if (playerSelection.toUpperCase() == "Scissors".toUpperCase() && computerSelection.toUpperCase() == "Paper".toUpperCase()) {
-        console.log("You win! Scissors beats Paper");
+    // Check if the player chose "scissors"
+    if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        const scissorsWin = document.createElement("p");
+        scissorsWin.textContent = "You win! Scissors beats Paper";
+        container.appendChild(scissorsWin);
         playerWin++;
-    } else if (playerSelection.toUpperCase() == "Scissors".toUpperCase() && computerSelection.toUpperCase() == "Scissors".toUpperCase()) {
-        console.log("It's a tie");
-    } else if (playerSelection.toUpperCase() == "Scissors".toUpperCase() && computerSelection.toUpperCase() == "Rock".toUpperCase()) {
-        console.log("You lose! Rock beats Scissors");
+    } else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
+        const scissorsTie = document.createElement("p");
+        scissorsTie.textContent = "It's a tie";
+        container.appendChild(scissorsTie);
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        const scissorsLose = document.createElement("p");
+        scissorsLose.textContent = "You lose! Rock beats Scissors";
+        container.appendChild(scissorsLose);
         computerWin++;
     }
 
